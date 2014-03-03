e83e11c19187bff0044feb02adb79b99d2d85875
ankit.gade <ankit.gade@vidushigoc.com>
2014-03-03 12:02:25 +0530

    Changes in config

--- config.php
+++ config.php
@@ -5,9 +5,6 @@
  */
 
 /* set error reporting */
-//ini_set('display_errors', 1);
-//error_reporting(E_ALL);
-//error_reporting(0);
 
 define('EZ_SLASHES', '/' );
 
@@ -30,26 +27,26 @@ define( 'EZ_DB_USER', 'ezigbtProdUser' );
 define( 'EZ_DB_PASS', 'DyfJjk4YzFl' );
 
 /* SMTP PORT */
-define( 'EZ_SMTP_PORT', 465 );
+define( 'EZ_SMTP_PORT', 587 );
 
 /* SMTP HOST */
-define( 'EZ_SMTP_HOST', "smtp.gmail.com" );
+define( 'EZ_SMTP_HOST', "smtp.live.com" );
 
 /* SMTP user */
-define( 'EZ_SMTP_USER', 'ezigbt@gmail.com' );
+define( 'EZ_SMTP_USER', 'support@ezigbt.com' );
 
 /* SMTP Password */
 define( 'EZ_SMTP_PASS', 'Goigbtgo!' );
 
 /* SMTP Reply To */
-define( 'EZ_SMTP_REPLY', 'ezigbt@gmail.com' ); // If user will reply, to whom it should go ?
+define( 'EZ_SMTP_REPLY', 'support@ezigbt.com' ); // If user will reply, to whom it should go ?
 
 /* SMTP Reply Name */
-define( 'EZ_SMTP_REPLY_NAME', 'ezIGBT' );
+define( 'EZ_SMTP_REPLY_NAME', 'EzIGBT' );
 
 /* SMTP Reply FROM */
 // From which address email is getting sent, On same address you will receive all emails
-define( 'EZ_SMTP_FROM', 'ezigbt@gmail.com' );
+define( 'EZ_SMTP_FROM', 'support@ezigbt.com' );
 
 /* SMTP Reply FROM Name */
-define( 'EZ_SMTP_REPLY_FROM_NAME', 'ezIGBT' );
+define( 'EZ_SMTP_REPLY_FROM_NAME', 'EzIGBT' );
